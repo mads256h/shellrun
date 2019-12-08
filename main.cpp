3bc0698cc24095c0334980b92d2c50f79b5745a4
Mads Mogensen <mads256h@gmail.com>
2019-12-08 23:41:46 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -76,7 +76,7 @@ int main(int argc, char **argv) {
   }
   if (((int)flag & (int)::flags::debug) == (int)flags::debug) {
     memcpy(shellcode + i, int3, sizeof(int3));
-    i += sizeof(int3);
+    i += sizeof(int3) + 10;
   }
 
   memcpy(shellcode + i, fileData, len);
