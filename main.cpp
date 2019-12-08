23e3c8ad6b9e42778358fd90d1bfd99abfa30927
Mads Mogensen <mads256h@gmail.com>
2019-12-08 20:44:47 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,15 +13,12 @@
 #include <Windows.h>
 #endif
 
-void print_usage() { puts("USAGE\nShellCodeRunner.exe file"); }
-
 int main(int argc, char **argv) {
   if (argc == 1 || argc > 2) {
     // report version
-    std::cout << argv[0] << " Version " << ShellCodeRunner_VERSION_MAJOR <<
-    "."
-              << ShellCodeRunner_VERSION_MINOR << std::endl;
-    std::cout << "Usage: " << argv[0] << " file" << std::endl;
+    std::cout << argv[0] << " Version " << ShellCodeRunner_VERSION_MAJOR << "."
+              << ShellCodeRunner_VERSION_MINOR << std::endl
+              << "Usage: " << argv[0] << " file" << std::endl;
     return 1;
   }
   const auto fs = std::filesystem::current_path() / argv[1];
@@ -36,8 +33,8 @@ int main(int argc, char **argv) {
 
 #if defined(WIN32) || defined(_WIN32) || defined(__WIN32)
   DWORD previous_access;
-  BOOL ret = VirtualProtect(data, len, PAGE_EXECUTE_READWRITE,
-                            &previous_access);
+  BOOL ret =
+      VirtualProtect(data, len, PAGE_EXECUTE_READWRITE, &previous_access);
 
   if (!ret) {
     printf("VirtualProtect\n");
