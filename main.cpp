8aa64b05f0b5d60c50f20f7a27134f3a3d46e1e0
Mads Mogensen <mads256h@gmail.com>
2019-12-08 22:31:03 +0100

    Add debugging and register clearing functionality

--- main.cpp
+++ main.cpp
@@ -12,6 +12,11 @@
 #include "config.h"
 #include "util.h"
 
+enum class flags {
+  debug = 1 << 0,
+  clear = 1 << 1,
+};
+
 int main(int argc, char **argv) {
   if (argc == 1) {
     // report version
@@ -19,6 +24,17 @@ int main(int argc, char **argv) {
               << ShellCodeRunner_VERSION_MINOR << std::endl
               << "Usage: " << argv[0] << "[flags] file" << std::endl;
     return 1;
+  }
+
+  flags flag;
+
+  for (int i = 1; i < argc - 1; i++) {
+    if (strcmp(argv[i], "--debug") == 0) {
+      flag = (::flags)((int)flag | (int)flags::debug);
+    }
+    if (strcmp(argv[i], "--clear") == 0) {
+      flag = (::flags)((int)flag | (int)flags::clear);
+    }
   }
 
   struct stat stat_buf;
@@ -30,19 +46,47 @@ int main(int argc, char **argv) {
 #else
   const auto pagesize = sysconf(_SC_PAGE_SIZE);
 
-  uint8_t *const fileData =
-      static_cast<uint8_t *>(memalign(pagesize, len));
+  uint8_t *const fileData = static_cast<uint8_t *>(memalign(pagesize, len));
 #endif
 
   std::ifstream file(argv[argc - 1], std::ifstream::binary);
   file.read((char *)&fileData[0], len);
   file.close();
 
-  MPROTECT(fileData, len);
+  auto final_len = len;
+  if (((int)flag & (int)::flags::debug) == (int)flags::debug) {
+    final_len += sizeof(int3);
+  }
+  if (((int)flag & (int)::flags::clear) == (int)flags::clear) {
+    final_len += sizeof(clear);
+  }
+  uint8_t *const shellcode = new uint8_t[final_len];
 
-  DEBUGBREAK();
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32)
+  uint8_t *const shellcode = new uint8_t[final_len];
+#else
+  const auto pagesize = sysconf(_SC_PAGE_SIZE);
 
-  ((void (*)(void))fileData)();
+  uint8_t *const shellcode =
+      static_cast<uint8_t *>(memalign(pagesize, final_len));
+#endif
+
+  size_t i = 0;
+
+  if (((int)flag & (int)::flags::clear) == (int)flags::clear) {
+    i += sizeof(clear);
+    memcpy(shellcode, clear, sizeof(clear));
+  }
+  if (((int)flag & (int)::flags::debug) == (int)flags::debug) {
+    i += sizeof(int3);
+    memcpy(shellcode + i, int3, sizeof(int3));
+  }
+
+  memcpy(shellcode + i, fileData, len);
+
+  MPROTECT(shellcode, len);
+
+  ((void (*)(void))shellcode)();
 
   return 0;
 }
