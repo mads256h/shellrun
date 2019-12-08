4ddf85e0f064b721ee2346b5a9cef0f56fbbb8b3
Mads Mogensen <mads256h@gmail.com>
2019-12-08 21:32:34 +0100

    test

--- main.cpp
+++ main.cpp
@@ -8,43 +8,30 @@
 
 #include "config.h"
 #include "util.h"
-
-#if defined(WIN32) || defined(_WIN32) || defined(__WIN32)
-#include <Windows.h>
-#endif
+#include "asm.h"
 
 int main(int argc, char **argv) {
-  if (argc == 1 || argc > 2) {
+  if (argc == 1) {
     // report version
     std::cout << argv[0] << " Version " << ShellCodeRunner_VERSION_MAJOR << "."
               << ShellCodeRunner_VERSION_MINOR << std::endl
-              << "Usage: " << argv[0] << " file" << std::endl;
+              << "Usage: " << argv[0] << "[flags] file" << std::endl;
     return 1;
   }
-  const auto fs = std::filesystem::current_path() / argv[1];
+
+  const auto fs = std::filesystem::current_path() / argv[argc - 1];
   const auto len = std::filesystem::file_size(fs);
-  uint8_t *const data = new uint8_t[len];
+  uint8_t *const fileData = new uint8_t[len];
 
   std::ifstream file(argv[1], std::ifstream::binary);
-
-  file.read((char *)&data[0], len);
-
+  file.read((char *)&fileData[0], len);
   file.close();
 
-#if defined(WIN32) || defined(_WIN32) || defined(__WIN32)
-  DWORD previous_access;
-  BOOL ret =
-      VirtualProtect(data, len, PAGE_EXECUTE_READWRITE, &previous_access);
-
-  if (!ret) {
-    printf("VirtualProtect\n");
-    return EXIT_FAILURE;
-  }
-#endif
+  MPROTECT(fileData, len);
 
   DEBUGBREAK();
 
-  ((void (*)(void))data)();
+  ((void (*)(void))fileData)();
 
   return 0;
 }
--- util.h
+++ util.h
@@ -1,7 +1,57 @@
+#ifndef _UTIL_H_
+#define _UTIL_H_
 #ifdef _MSC_VER
-#include <intrins.h>
-#define DEBUGBREAK() __debugbreak()
+#define DEBUGBREAK() __asm {int 3}
 #endif
 #ifdef __GNUC__
 #define DEBUGBREAK() asm("int3")
+#endif
+
+// Check windows
+#if _WIN32 || _WIN64
+#if _WIN64
+#define ENVIRONMENT64
+#else
+#define ENVIRONMENT32
+#endif
+#endif
+
+// Check GCC
+#if __GNUC__
+#if __x86_64__ || __ppc64__
+#define ENVIRONMENT64
+#else
+#define ENVIRONMENT32
+#endif
+#endif
+
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32)
+#include <Windows.h>
+#define MPROTECT(shellcode, length)                                            \
+  {                                                                            \
+    DWORD previous_access;                                                     \
+    BOOL ret = VirtualProtect(shellcode, length, PAGE_EXECUTE_READWRITE,       \
+                              &previous_access);                               \
+                                                                               \
+    if (!ret) {                                                                \
+      printf("VirtualProtect\n");                                              \
+      return EXIT_FAILURE;                                                     \
+    }                                                                          \
+  }
+#else
+#include <unistd.h>
+#include <sys/mman.h>
+#define MPROTECT(shellcode, length)                                            \
+  {                                                                            \
+    long pagesize = sysconf(_SC_PAGE_SIZE);                                    \
+    long page_no = (long)shellcode / pagesize;                                 \
+    int res = mprotect((void *)(page_no * pagesize), (long)page_no + length,   \
+                       PROT_EXEC | PROT_READ | PROT_WRITE);                    \
+    if (res) {                                                                 \
+      fprintf(stderr, "mprotect error:%d\n", res);                             \
+      return 1;                                                                \
+    }                                                                          \
+  }
+#endif
+
 #endif
