7dbd896dc871805bb99dae85199570d7290de783
Mads Mogensen <mads256h@gmail.com>
2019-12-10 01:08:01 +0100

    Comments

--- ShellcodeExamples/Linux/X86/HelloWorld.asm
+++ ShellcodeExamples/Linux/X86/HelloWorld.asm
@@ -10,15 +10,17 @@ call MainCode
 	
  
 MainCode:
-    pop esi
+    ; Print hello world to stdout
+    pop esi ; Get eip into esi
 	mov eax, SYS_WRITE
 	mov ebx, STD_OUTPUT
-	lea ecx, [msg - MainCode + esi]
-	mov edx, msglen
+	lea ecx, [msg - MainCode + esi] ; Load msg by using relative indexing.
+	mov edx, msglen ; msglen is just a constant so no relative indexing is needed.
 	int 0x80
  
+    ; Exit with return value 0
 	mov eax, SYS_EXIT
-	mov ebx, 0
+	mov ebx, 0 ; return value
 	int 0x80
 
     msg: db `Hello World!\n`
