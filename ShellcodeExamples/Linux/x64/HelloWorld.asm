cac768872af446f56b8502bb64d089e4bdef0b7f
Mads Mogensen <mads256h@gmail.com>
2020-02-05 13:20:54 +0100

    Fix formatting and add comments

--- ShellcodeExamples/Linux/x64/HelloWorld.asm
+++ ShellcodeExamples/Linux/x64/HelloWorld.asm
@@ -6,22 +6,22 @@ STD_OUTPUT equ 1
 
 section .text
 global start
-global _start
 
 start:
-_start:
     call MainCode
 
 MainCode:
-    pop rsi
+    ; Print hello world to stdout
+    pop rsi ; Get rip into rsi
     mov rax, SYS_WRITE
     mov rdi, STD_OUTPUT
-    lea rsi, [msg - MainCode + rsi]
-    mov rdx, msglen
+    lea rsi, [msg - MainCode + rsi] ; Load msg by using relative indexing.
+    mov rdx, msglen ; msglen is just a constant so no relative indexing is needed.
     syscall
 
+    ; Exit with return value 0
     mov rax, SYS_EXIT
-    mov rdi, 0
+    mov rdi, 0 ; return value
     syscall
 
     msg: db `Hello world!\n`
--- ShellcodeExamples/Linux/x86/HelloWorld.asm
+++ ShellcodeExamples/Linux/x86/HelloWorld.asm
@@ -9,21 +9,20 @@ global start
  
 start:
     call MainCode
-	
- 
+
 MainCode:
     ; Print hello world to stdout
     pop esi ; Get eip into esi
-	mov eax, SYS_WRITE
-	mov ebx, STD_OUTPUT
-	lea ecx, [msg - MainCode + esi] ; Load msg by using relative indexing.
-	mov edx, msglen ; msglen is just a constant so no relative indexing is needed.
-	int 0x80
- 
-    ; Exit with return value 0
-	mov eax, SYS_EXIT
-	mov ebx, 0 ; return value
-	int 0x80
+    mov eax, SYS_WRITE
+    mov ebx, STD_OUTPUT
+    lea ecx, [msg - MainCode + esi] ; Load msg by using relative indexing.
+    mov edx, msglen ; msglen is just a constant so no relative indexing is needed.
+    int 0x80
 
+    ; Exit with return value 0
+    mov eax, SYS_EXIT
+    mov ebx, 0 ; return value
+    int 0x80
+    
     msg: db `Hello World!\n`
-	msglen equ $-msg
+    msglen equ $-msg
